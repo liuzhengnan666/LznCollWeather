7320d2df2fd885c942f0e74da062f08343dc0a46
lzn <lzn@qq.com>
2017-03-24 16:05:02 +0800

    加入数据库LitePal和表的配置

---
+++ app/src/main/java/cn/liu/collweather/db/City.java
@@ -0,0 +1,49 @@
+package cn.liu.collweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2017-3-24.
+ */
+
+public class City extends DataSupport {
+    private int id;
+
+    private String cityName;
+
+    private int cityCode;
+
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
